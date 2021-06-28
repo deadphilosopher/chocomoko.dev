5c36bc0b9bef34572a26d30967e3b8a9fb1ae0a1
Ryan C <{ID}+{username}@users.noreply.github.com>
2021-06-27 19:57:10 -0700

    Updated base image to Nginx

--- chocomoko-web/exec.sh
+++ chocomoko-web/exec.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 # Execute environment substitution
-#envsubst '$WEB_SERVER_PORT' < /etc/nginx/sites-available/default.template > /etc/nginx/sites-available/default
 envsubst '$WEB_SERVER_PORT' < /var/tmp/default.template > /etc/nginx/sites-available/default
 
 # Execute commands that are passed
